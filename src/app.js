f33e15fbac194b9a8fea9f29c23ab6aa552f787f
AlisonCastroM <alisoncastro7616@gmail.com>
2024-10-04 22:50:18 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,44 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+  document.querySelector("button").addEventListener("click", generarCartaNueva);
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let cardValue = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "A",
+    "J",
+    "Q",
+    "K"
+  ];
+  function generarCartaNueva() {
+    let cardPinta = ["♦", "♥", "♠", "♣"];
+
+    function elegirAleatorio(array) {
+      return array[Math.floor(Math.random() * array.length)];
+    }
+    const valorAleatorio = elegirAleatorio(cardValue);
+
+    document.querySelector(".value").innerHTML = valorAleatorio;
+
+    const pintaAleatoria = elegirAleatorio(cardPinta);
+    document.querySelectorAll(".pinta").forEach(elementoPinta => {
+      elementoPinta.innerHTML = pintaAleatoria;
+
+      if (pintaAleatoria == "♥" || pintaAleatoria == "♦") {
+        elementoPinta.style.color = "red";
+      }
+      if (pintaAleatoria == "♠" || pintaAleatoria == "♣") {
+        elementoPinta.style.color = "black";
+      }
+    });
+  }
+  generarCartaNueva();
 };
